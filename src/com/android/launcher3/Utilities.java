65f63c28680b53a673e724a868c933b7a4b35f86
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-04 21:25:40 +0800

    Utilities: Fix C2S enablement check
    bug: https://github.com/AxionAOSP/issue_tracker/issues/12
    Change-Id: I8bc3545f1df63dbde868aaeb3c7379898bf07b29

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -991,7 +991,7 @@ public final class Utilities {
         Context appContext = context.getApplicationContext();
         ContextualSearchManager contextualSearchManager = 
             (ContextualSearchManager) appContext.getSystemService(Context.CONTEXTUAL_SEARCH_SERVICE);
-        if (contextualSearchManager == null) {
+        if (contextualSearchManager == null || !isLongPressSearchEnabled(context)) {
             return false;
         }
         try {
@@ -1002,8 +1002,9 @@ public final class Utilities {
     }
     
     public static boolean isLongPressSearchEnabled(Context context) {
-        return Settings.Secure.getInt(
-            context.getApplicationContext().getContentResolver(), "search_press_hold_nav_handle_enabled", 1) == 1;
+        return Settings.Secure.getInt(context.getContentResolver(),
+                Settings.Secure.SEARCH_ALL_ENTRYPOINTS_ENABLED, 1)
+                == 1;
     }
 
     public static boolean showQuickspace(Context context) {
