308cc6a6794836aa8d24b48098c43cfe302009c6
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-06 18:59:06 +0800

    Utilities: add tablet utility methods
    Change-Id: I63edc6a55e928df59137ae930e467337f18b1005

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1046,4 +1046,14 @@ public final class Utilities {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_ALLOW_WALLPAPER_ZOOMING, true);
     }
+    
+    public static boolean isTaskbarEnabled(Context context) {
+        return com.android.launcher3.util.SettingsCache.INSTANCE.get(context).getValue(com.android.launcher3.taskbar.TaskbarManager.ENABLE_TASKBAR,
+                        isTablet(context) ? 1 : 0);
+    }
+    
+    public static boolean isTablet(Context context) {
+        int smallestWidthDp = context.getResources().getConfiguration().smallestScreenWidthDp;
+        return smallestWidthDp >= 600;
+    }
 }
